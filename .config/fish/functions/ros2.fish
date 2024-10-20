0dc19393b529e7373e1642e6e3ae91135bcf8e59
amoehring99 <github@amoehring.com>
2024-10-20 02:17:13 +0200

    changed ros2box alias to be used with every command

--- .config/fish/functions/ros2.fish
+++ .config/fish/functions/ros2.fish
@@ -1,4 +1,4 @@
 function ros2 --wraps=ros2box --description 'alias ros2=ros2box'
-  ros2box $argv
+  ros2box ros2 $argv
         
 end
--- .config/fish/functions/ros2box.fish
+++ .config/fish/functions/ros2box.fish
@@ -1,3 +1,3 @@
 function ros2box
-    distrobox-enter -n ubuntu-22-04 -- bash -c "source /opt/ros/humble/setup.bash && ros2 $argv"
+    distrobox-enter -n ubuntu-22-04 -- bash -c "source /opt/ros/humble/setup.bash && $argv"
 end
